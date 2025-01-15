a950e5b0327f346704199515200b92724c05f536
RafaelSantiagoChiriviPena <158981100+RafaelSantiagoChiriviPena@users.noreply.github.com>
2025-01-14 21:51:17 -0500

    Add files via upload

---
+++ reto_5/reto_5_punto_2/Shape/shape.py
@@ -0,0 +1,11 @@
+class Shape:
+    """Se define una clase general con metodos planos para poder ser sobreescritos en las clases heredadas"""
+
+    def __init__(self):
+        pass
+    def compute_area(self):
+        pass
+    def compute_perimeter(self):
+        pass
+    def compute_inner_angles(self):
+        pass
